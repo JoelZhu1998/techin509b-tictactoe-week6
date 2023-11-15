0478c08720cc36e131c703a29433a45d7d01f08f
JoelZhu1998 <148395346+JoelZhu1998@users.noreply.github.com>
2023-11-14 22:27:43 -0800

    Update CLI.py

--- CLI.py
+++ CLI.py
@@ -1,8 +1,5 @@
-# This file contains the Command Line Interface (CLI) for
-# the Tic-Tac-Toe game. This is where input and output happens.
-# For core game logic, see logic.py.
-
 import logging
+import random  # Import the random module
 from logic import make_empty_board, get_winner, other_player
 
 logging.basicConfig(
@@ -10,39 +7,85 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-def print_board(board):
-    for i in range(3):
-        for j in range(3):
-            print(" " if board[i][j] is None else board[i][j], end="")
-            if j < 2:
-                print(" | ", end="")
-        print()
-        if i < 2:
-            print("-" * 9)
+class Player:
+    def __init__(self, symbol):
+        self.symbol = symbol
 
-def main():
-    board = make_empty_board()
-    player = 'X'
-    winner = None
+    def make_move(self, board):
+        raise NotImplementedError("Subclasses must implement the make_move method.")
 
-    while winner is None:
-        print_board(board)
-        print(f"Player {player}'s turn:")
+class HumanPlayer(Player):
+    def make_move(self, board):
         row = int(input("Enter the row (0, 1, 2): "))
         col = int(input("Enter the column (0, 1, 2): "))
+        return row, col
 
-        if board[row][col] is None:
-            board[row][col] = player
-            winner = get_winner(board)
-            player = other_player(player)
+class BotPlayer(Player):
+    def make_move(self, board):
+        available_moves = [(i, j) for i in range(3) for j in range(3) if board[i][j] is None]
+        return random.choice(available_moves) if available_moves else None
+
+class TicTacToeGame:
+    def __init__(self, player1, player2):
+        self.board = make_empty_board()
+        self.players = [player1, player2]
+        self.current_player_index = 0
+        self.winner = None
+
+    def switch_player(self):
+        self.current_player_index = (self.current_player_index + 1) % 2
+
+    def print_board(self):
+        for i in range(3):
+            for j in range(3):
+                print(" " if self.board[i][j] is None else self.board[i][j], end="")
+                if j < 2:
+                    print(" | ", end="")
+            print()
+            if i < 2:
+                print("-" * 9)
+
+    def play_round(self):
+        player = self.players[self.current_player_index]
+        self.print_board()  # Corrected line
+        print(f"Player {player.symbol}'s turn:")
+
+        move = player.make_move(self.board)
+
+        if move is not None:
+            row, col = move
+            if self.board[row][col] is None:
+                self.board[row][col] = player.symbol
+                self.winner = get_winner(self.board)
+            else:
+                print("Invalid move. That position is already occupied. Try again.")
         else:
-            print("Invalid move. That position is already occupied. Try again.")
+            print("Invalid move. No available moves. Try again.")
 
-    print_board(board)
-    if winner:
-        print(f"Player {winner} wins!")
-        logging.info('You win!')
-    else:
+        self.switch_player()
+
+    def play_game(self):
+        while self.winner is None:
+            self.play_round()
+
+        self.print_board()
+
+        if self.winner:
+            print(f"Player {self.winner} wins!")
+            logging.info('You win!')
+        else:
+            print("It's a draw!")
+            logging.info("It's a draw!")
+
+if __name__ == '__main__':
+    player1_symbol = 'X'
+    player2_symbol = 'O'
+
+    player1 = HumanPlayer(player1_symbol)
+    player2 = BotPlayer(player2_symbol)
+
+    game = TicTacToeGame(player1, player2)
+    game.play_game()
         print("It's a draw!")
         logging.info("It's a draw!")
 
